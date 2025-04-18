cbaa5e02bcbc72d88974d75eabc5b047942f7b89
liuhuihui <627146930@qq.com>
2025-04-18 21:44:57 +0800

    用户界面 - 商品详情页

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -1,14 +1,5 @@
 <?php
 
-declare(strict_types=1);
-/**
- * Created by PhpStorm.
- * User: SillyCat
- * Date: 2025-04-18
- * Time: 21:38
- */
-
-
 namespace App\Admin\Controllers;
 
 use App\Models\Product;
@@ -26,6 +17,7 @@ class ProductsController extends Controller
 
     /**
      * Index interface.
+     *
      * @return Content
      */
     public function index()
@@ -38,35 +30,34 @@ class ProductsController extends Controller
 
     /**
      * Edit interface.
+     *
      * @param $id
      * @return Content
      */
     public function edit($id)
     {
         return Admin::content(function (Content $content) use ($id) {
-            $content->header('header');
-            $content->description('description');
-
+            $content->header('编辑商品');
             $content->body($this->form()->edit($id));
         });
     }
 
     /**
      * Create interface.
+     *
      * @return Content
      */
     public function create()
     {
         return Admin::content(function (Content $content) {
-            $content->header('header');
-            $content->description('description');
-
+            $content->header('创建商品');
             $content->body($this->form());
         });
     }
 
     /**
      * Make a grid builder.
+     *
      * @return Grid
      */
     protected function grid()
@@ -90,15 +81,32 @@ class ProductsController extends Controller
 
     /**
      * Make a form builder.
+     *
      * @return Form
      */
     protected function form()
     {
+        // 创建一个表单
         return Admin::form(Product::class, function (Form $form) {
-            $form->display('id', 'ID');
-
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
+            $form->text('title', '商品名称')->rules('required');
+            // 创建一个选择图片的框
+            $form->image('image', '封面图片')->rules('required|image');
+            // 创建一个富文本编辑器
+            $form->editor('description', '商品描述')->rules('required');
+            // 创建一组单选框
+            $form->radio('on_sale', '上架')->options(['1' => '是', '0'=> '否'])->default('0');
+            // 直接添加一对多的关联模型
+            $form->hasMany('skus', function (Form\NestedForm $form) {
+                $form->text('title', 'SKU 名称')->rules('required');
+                $form->text('description', 'SKU 描述')->rules('required');
+                $form->text('price', '单价')->rules('required|numeric|min:0.01');
+                $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+            });
+            // 定义事件回调，当模型即将保存时会触发这个回调
+            $form->saving(function (Form $form) {
+                $form->model()->price = collect($form->skus)->min('price');
+            });
         });
     }
 }
