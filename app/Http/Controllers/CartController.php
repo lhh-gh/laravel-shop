7e1c83b718731deeb8ca7f66cab4ecb0d46f88c6
liuhuihui <627146930@qq.com>
2025-04-22 17:04:10 +0800

    创建订单

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductSku;
 use Illuminate\Http\Request;
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
@@ -34,6 +35,19 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+
+        return [];
+    }
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
+
+        return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
+    }
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
 
         return [];
     }
